349d85454fcb94f4164d8db49fe7a33979ec600e
Luís Henrique dos Reis <luik98@gmail.com>
2018-10-11 10:26:20 -0300

    Update index.js

--- Three/js/index.js
+++ Three/js/index.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 2.5, 5.5 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 2.5, 7.5 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -8,7 +8,7 @@ document.body.appendChild( renderer.domElement );
 var geometry = new THREE.Geometry();
 
 // VARS
-var resolution = 8;
+var resolution = 16;
 var radius = 2;
 // /VARS
 
@@ -19,45 +19,40 @@ function SphereVertex(radius, theta, phi) {
         radius * Math.cos(theta) * Math.sin(phi))
 }
 
-var resolutionSquared = Math.pow(resolution, 2);
+var verticesAmount = resolution * (resolution + 1);
 
 // This code is flawed for cases where there is a vertex at the Y axis, since it will create redundant vertices.
-for (let i = 0; i < resolution; i++) {
+for (let i = 0; i <= resolution; i++) {
     // In the current implementation, phi must always be 2PI, else there will be misplaced vertices and faces.
     let phi = 2 * Math.PI * i / resolution
     
-    // Should start with j at 1 so that the "bottom" vertex does not repeat.
-    for (let j = 0; j < resolution; j++) {
+    for (let j = 0; j <= resolution; j++) {
         let theta = -Math.PI / 2 + (Math.PI * j / resolution)
         
-        geometry.vertices.push(SphereVertex(radius, theta, phi));
+        if (i < resolution) { geometry.vertices.push(SphereVertex(radius, theta, phi)); }
 
-        if (j < resolution - 1) {
-            console.log("i: "+i);
-            console.log("j: "+j);
-            console.log("");
-            geometry.faces.push(new THREE.Face3(
-                ((i * resolution) + j) % resolutionSquared,                 // X+0, Y+0
-                ((i * resolution) + j + 1) % resolutionSquared,             // X+0, Y+1
-                ((i * resolution) + j + resolution) % resolutionSquared));  // X+1, Y+0
-            geometry.faces.push(new THREE.Face3(
-                ((i * resolution) + j + 1) % resolutionSquared,             // X+0, Y+1
-                ((i * resolution) + j + resolution + 1) % resolutionSquared,// X+1, Y+1
-                ((i * resolution) + j + resolution) % resolutionSquared));  // X+1, Y+0
-        }
+        let v00 = ((i * (resolution)) + j) % verticesAmount;
+        let v01 = ((i * (resolution)) + j + 1) % verticesAmount;
+        let v10 = ((i * (resolution)) + j + resolution + 1) % verticesAmount;
+        let v11 = ((i * (resolution)) + j + resolution + 2) % verticesAmount;
+
+        geometry.faces.push(new THREE.Face3(
+            v00,    // X+0, Y+0
+            v01,    // X+0, Y+1
+            v10));  // X+1, Y+0
+        geometry.faces.push(new THREE.Face3(
+            v01,    // X+0, Y+1
+            v11,    // X+1, Y+1
+            v10));  // X+1, Y+0
     }
-    // console.log("i: "+i);
-    // geometry.faces.push(new THREE.Face3(
-    //     ((i * resolution) + resolution - 1) % resolutionSquared,
-    //     ((i * resolution) - 1) % resolutionSquared,
-    //     resolutionSquared));
 }
 
 var material = new THREE.MeshDepthMaterial( { color: 0xffffff } );
 var mesh = new THREE.Mesh(geometry, material);
 
-//var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-//var cube = new THREE.Mesh( geometry, material );
+// var material = new THREE.PointsMaterial( { color: 0xffffff, size: 0.1 } );
+// var mesh = new THREE.Points(geometry, material);
+
 scene.add( mesh );
 
 camera.position.z = 5;
@@ -66,7 +61,7 @@ var animate = function () {
     requestAnimationFrame( animate );
 
     mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
+    mesh.rotation.y += 0.02;
 
     renderer.render( scene, camera );
 };
